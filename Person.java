3e99f911906a610feb95075df8a752e9185645b7
KongCPH <112859698+KongCPH@users.noreply.github.com>
2024-10-01 13:29:04 +0200

    Update Person.java
    some formatting

--- Person.java
+++ Person.java
@@ -1,6 +1,6 @@
 public class Person {
- private String firstName = "Tess";  private String lastName = “Gaston"; 
- //The constructor method
- public Person(){
- }
+   private String firstName = "Tess";  private String lastName = “Gaston"; 
+   //The constructor method
+   public Person(){
+   }
 }
